ce2964a6f3c3125c086245273d9454461c4f0bfa
Roman Kyryliuk <spielerua@gmail.com>
2023-05-23 23:30:33 +0300

    feat(clean): hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,7 @@ import "hardhat-gas-reporter";
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL ||
     "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY";
-const PRIVATE_KEY =
-    process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "private key";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "ETHERSCAN_KEY";
 
 const config: HardhatUserConfig = {
